e273e2a9216df23a69ca60422ee2679bf15b010a
Eduard Bess <eduard.bess@gmail.com>
2018-11-08 18:05:42 +0100

    tabs to spaces

--- src/AdvancedNovaMediaLibraryServiceProvider.php
+++ src/AdvancedNovaMediaLibraryServiceProvider.php
@@ -10,7 +10,7 @@ class AdvancedNovaMediaLibraryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-	    Nova::serving(function (ServingNova $event) {
+        Nova::serving(function (ServingNova $event) {
             Nova::script('media-lib-images-field', __DIR__.'/../dist/js/field.js');
         });
     }
