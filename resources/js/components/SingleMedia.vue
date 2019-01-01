3c4055b75b970a1072ce98226a86963cca6fa407
Ibrahim Mubarak <ibrahim@faimtech.in>
2019-01-01 14:51:45 +0530

    Add support for Video Thumbnail generation

--- resources/js/components/SingleMedia.vue
+++ resources/js/components/SingleMedia.vue
@@ -2,13 +2,13 @@
   <gallery-item class="gallery-item-image">
     <div class="gallery-item-info p-3">
       <a v-if="removable" class="delete" href="#" @click.prevent="$emit('remove')">
-        <icon type="delete" view-box="0 0 20 20" width="16" height="16" />
+        <icon type="delete" view-box="0 0 20 20" width="16" height="16"/>
       </a>
       <a v-if="isCustomPropertiesEditable" class="edit" href="#" @click.prevent="$emit('editCustomProperties')">
-        <icon type="edit" view-box="0 0 20 20" width="16" height="16" />
+        <icon type="edit" view-box="0 0 20 20" width="16" height="16"/>
       </a>
       <a class="preview" :href="image.full_urls.default" target="_blank">
-        <icon type="search" view-box="0 0 20 20" width="30" height="30" />
+        <icon type="search" view-box="0 0 20 20" width="30" height="30"/>
       </a>
     </div>
     <img :src="src" :alt="image.name" class="gallery-image">
@@ -23,24 +23,57 @@
       GalleryItem,
     },
     props: ['image', 'field', 'removable', 'isCustomPropertiesEditable'],
-    computed: {
-      src() {
+    data() {
+      return {
+        src: "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
+      }
+    },
+    watch: {
+      image: {
+        handler: 'getImage',
+        immediate: true
+      }
+    },
+    methods: {
+      getImage() {
         // Return desired image conversion on view if it exists
         let conversionOnView = this.field.conversionOnView;
 
         if (this.image.id && conversionOnView && this.image.full_urls[conversionOnView]) {
-          return this.image.full_urls[conversionOnView];
+          this.src = this.image.full_urls[conversionOnView];
         }
-        
-        // Return thumnail if conversion exists
+
+        // Return thumbnail if conversion exists
         let thumbnail = this.field.thumbnail;
 
         if (this.image.id && thumbnail && this.image.full_urls[thumbnail]) {
-          return this.image.full_urls[thumbnail];
+          this.src = this.image.full_urls[thumbnail];
         }
 
-        return this.image.full_urls.default;
+        if (this.image.full_urls.default.startsWith("data:video")) {
+          //Seconds to seek to, to get thumbnail of video
+          let seconds = 1;  //TODO get this from the field instead of hardcoding it here
+          this.getVideoThumbnail(this.image.full_urls.default, seconds);
+          return;
+        }
+
+        this.src = this.image.full_urls.default;
       },
+      getVideoThumbnail(path, secs = 0) {
+        const video = document.createElement('video');
+        video.onloadedmetadata = () => {
+          video.currentTime = Math.min(Math.max(0, (secs < 0 ? video.duration : 0) + secs), video.duration);
+        };
+        video.onseeked = (e) => {
+          const canvas = document.createElement('canvas');
+          canvas.height = video.videoHeight;
+          canvas.width = video.videoWidth;
+          const ctx = canvas.getContext('2d');
+          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+          this.src = canvas.toDataURL();
+        };
+        video.src = path;
+      }
     },
   };
 </script>
