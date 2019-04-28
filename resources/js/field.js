e59d39bbc06ebf5754d736a7f28fe782dfb759b8
Ben Casey <bcasey@tigerstrikemedia.com>
2019-04-28 21:59:32 +0930

    Add .default to imports

--- resources/js/field.js
+++ resources/js/field.js
@@ -3,7 +3,7 @@ import VuejsClipper from 'vuejs-clipper';
 Nova.booting((Vue, router) => {
   Vue.use(VuejsClipper);
 
-  Vue.component('index-advanced-media-library-field', require('./components/fields/IndexField'));
-  Vue.component('detail-advanced-media-library-field', require('./components/fields/DetailField'));
-  Vue.component('form-advanced-media-library-field', require('./components/fields/FormField'));
+  Vue.component('index-advanced-media-library-field', require('./components/fields/IndexField').default);
+  Vue.component('detail-advanced-media-library-field', require('./components/fields/DetailField').default);
+  Vue.component('form-advanced-media-library-field', require('./components/fields/FormField').default);
 });
