8704d7028aa35714f2349cd55e9c8124f11babdb
Gareth Somers <garethsomers3@gmail.com>
2019-08-29 14:28:07 +0100

    Added support for Laravel Scout

--- src/Http/Controllers/MediaController.php
+++ src/Http/Controllers/MediaController.php
@@ -10,23 +10,29 @@ class MediaController extends Controller
     public function index(MediaRequest $request)
     {
         $media_class = config('medialibrary.media_model');
+        $media_class_is_searchable = method_exists($media_class, 'search');
 
         $search_text = $request->input('search_text') ?: null;
         $per_page = $request->input('per_page') ?: 18;
         $page = $request->input('page') ?: 1;
 
-        $query = $media_class::query();
+        $query = null;
 
-        if ($search_text) {
-            $query->where(function ($query) use($search_text) {
-                $query->where('name', 'LIKE', '%'.$search_text.'%');
-                $query->orWhere('file_name', 'LIKE', '%'.$search_text.'%');
+        if ($search_text && $media_class_is_searchable) {
+            $query = $media_class::search($search_text);
+        } else if ($search_text && !$media_class_is_searchable) {
+            $query = $media_class::query();
+            $query->where(function ($query) use ($search_text) {
+                $query->where('name', 'LIKE', '%' . $search_text . '%');
+                $query->orWhere('file_name', 'LIKE', '%' . $search_text . '%');
             });
+            $query->latest();
+        } else {
+            $query = $media_class::query();
+            $query->latest();
         }
 
-        $query->latest();
-
-        $results = $query->paginate($per_page, ['*'], 'page', $page);
+        $results = $query->paginate($per_page);
 
         return MediaResource::collection($results);
     }
