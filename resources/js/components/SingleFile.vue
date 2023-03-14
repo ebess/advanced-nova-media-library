7b39d682861e04005330bca511e87f35916178bb
Potsky <potsky@me.com>
2023-03-14 13:37:44 +0100

    Fix icon on Files field

--- resources/js/components/SingleFile.vue
+++ resources/js/components/SingleFile.vue
@@ -2,19 +2,19 @@
   <gallery-item class="gallery-item-file">
     <div class="gallery-item-info">
       <a class="download mr-2" :href="image.__media_urls__.__original__" target="_blank">
-        <icon type="search" view-box="0 0 20 20" width="16" height="16" />
+        <Icon type="search" view-box="0 0 20 20" width="16" height="16" />
       </a>
       <a v-if="downloadUrl" class="download mr-2" :href="downloadUrl">
-        <icon type="download" view-box="0 0 20 22" width="16" height="16" />
+        <Icon type="download" view-box="0 0 20 22" width="16" height="16" />
       </a>
       <span class="label">
         {{ image.file_name }}
       </span>
       <a v-if="isCustomPropertiesEditable" class="edit edit--file ml-2" href="#" @click.prevent="$emit('edit-custom-properties')">
-        <icon type="edit" view-box="0 0 20 20" width="16" height="16" />
+        <Icon type="pencil" view-box="0 0 20 20" width="16" height="16" />
       </a>
       <a v-if="removable" class="delete ml-2" href="#" @click.prevent="$emit('remove')">
-        <icon type="delete" view-box="0 0 20 20" width="16" height="16" />
+        <Icon type="trash" view-box="0 0 20 20" width="16" height="16" />
       </a>
     </div>
   </gallery-item>
