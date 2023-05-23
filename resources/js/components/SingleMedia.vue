5923fd3df37a58afc90a6b5cc1d51b3854ec5604
Potsky <potsky@me.com>
2023-05-23 10:45:24 +0200

    Use UUID to protect file downloads

--- resources/js/components/SingleMedia.vue
+++ resources/js/components/SingleMedia.vue
@@ -50,7 +50,7 @@
     }),
     computed: {
       downloadUrl() {
-        return this.image.id ? `/nova-vendor/ebess/advanced-nova-media-library/download/${this.image.id}` : null;
+        return this.image.id ? `/nova-vendor/ebess/advanced-nova-media-library/download/${this.image.id}?uuid=${this.image.uuid}` : null;
       },
       croppable() {
         return this.editable &&
