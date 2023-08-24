83b8410fe63b607dc7eda6b990cef33f971a6f9f
Leandro Gehlen <leandrogehlen@gmail.com>
2023-08-24 05:28:55 -0300

    Fix issues loading translations (#391)
    * Fix issues loading translations
    
    * Replace `resource_path` by `lang_path`

--- src/AdvancedNovaMediaLibraryServiceProvider.php
+++ src/AdvancedNovaMediaLibraryServiceProvider.php
@@ -19,7 +19,10 @@ class AdvancedNovaMediaLibraryServiceProvider extends ServiceProvider
             $this->routes();
         });
 
+        $this->loadTranslations();
+
         Nova::serving(function (ServingNova $event) {
+            $this->bootTranslations();
             Nova::script('media-lib-images-field', __DIR__.'/../dist/js/field.js');
         });
     }
@@ -33,5 +36,29 @@ class AdvancedNovaMediaLibraryServiceProvider extends ServiceProvider
         Route::middleware(['nova'])
             ->prefix('nova-vendor/ebess/advanced-nova-media-library')
             ->group(__DIR__.'/../routes/api.php');
+    }
+
+    /**
+     * Load package translation resources.
+     *
+     * @return void
+     */
+    protected function loadTranslations()
+    {
+        $this->loadJSONTranslationsFrom(__DIR__ . '/../resources/lang');
+        $this->loadJSONTranslationsFrom(lang_path('vendor/advanced-nova-media-library'));
+    }
+
+    /**
+     * Bootstraps current application locale translations to Nova.
+     *
+     * @return void
+     */
+    protected function bootTranslations()
+    {
+        $locale = $this->app->getLocale();
+
+        Nova::translations(__DIR__ . "/../resources/lang/{$locale}.json");
+        Nova::translations(lang_path("vendor/advanced-nova-media-library/$locale.json"));
     }
 }
