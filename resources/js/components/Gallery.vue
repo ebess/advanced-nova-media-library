262cfb33dd31e98f8cdd58d487a92442fe0360c7
Vincent Girol <vincent@girol.fr>
2024-05-17 15:26:43 +0200

    Display toast message when file type or file size validation fail

--- resources/js/components/Gallery.vue
+++ resources/js/components/Gallery.vue
@@ -225,7 +225,7 @@ export default {
     },
     validateFileSize(file) {
       if (this.field.maxFileSize && ((file.size / 1024) > this.field.maxFileSize)) {
-        this.$toasted.error(this.__(
+        Nova.error(this.__(
           'Maximum file size is :amount MB',
           {amount: String(this.field.maxFileSize / 1024)}
         ));
@@ -244,7 +244,7 @@ export default {
         }
       }
 
-      this.$toasted.error(this.__(
+      Nova.error(this.__(
         'File type must be: :types',
         {types: this.field.allowedFileTypes.join(' / ')}
       ));
